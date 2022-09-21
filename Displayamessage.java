1aa15d78d8741331f24b6d7b17ce0a9be325e87f
anki0221 <ankitsanodiya31081994@gmail.comgit config --global user.email ankitsanodiya31081994@gmail.com>
2022-09-22 00:55:24 +0530

    Day1problems

---
+++ Displayamessage.java
@@ -0,0 +1,10 @@
+package Day1problems;
+
+public class Displayamessage {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world java");
+
+	}
+
+}
---
+++ Static.java
@@ -0,0 +1,18 @@
+package Day1problems;
+
+public class Static {
+
+static int a=50;
+static int b=50;
+static int c= a + b;
+
+
+static void fun() {
+System.out.println("Static variable sum is :" + c);
+}
+	public static void main(String[]args) {
+  fun (); 
+	
+	}
+
+}

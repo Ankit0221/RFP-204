6f1394188277ef8b3eea284b9d481d02d5ebc1a9
Ankit0221 <112364607+Ankit0221@users.noreply.github.com>
2022-09-29 15:57:45 +0530

    Day5problems

---
+++ Day5_6_remainder.java
@@ -0,0 +1,21 @@
+package Day5_problems;
+
+public class Day5_6_remainder {
+
+	
+
+		public static void main(String[] args) {
+		
+		int dividend = 24 ;
+		int divisor = 4 ;
+		int quotient=dividend / divisor;
+		int remainder = dividend%divisor;
+		
+		
+		
+		System.out.println("Quo :"+quotient);
+	    System.out.println("Rem :"+remainder);
+		}
+
+	}
+

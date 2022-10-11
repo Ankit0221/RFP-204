9e5c210f3b57861d4eede5222a36751c60e20f10
Ankit0221 <112364607+Ankit0221@users.noreply.github.com>
2022-10-11 08:47:50 +0530

    Day4problems

---
+++ Day4Employeeuc1.java
@@ -0,0 +1,19 @@
+package Day4_Employee_snake;
+
+public class Day4Employeeuc1 {
+
+	    public static void main(String args[])
+	    {
+	        int fulltime = 1;
+	        int employee = (int)(Math.random()*100)%2;
+	        if (employee == fulltime)
+	        {
+	            System.out.println("employee is present ");
+
+	        } else
+	        {
+	            System.out.println("employee is absent ");
+
+	        }
+	    }
+	}
